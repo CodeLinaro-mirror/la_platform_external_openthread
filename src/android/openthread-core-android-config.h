9318b960e28fb63fcf2972760793f69aa523704d
Li Cao <irvingcl@google.com>
2025-12-09 23:39:07 -0800

    Fix the comment of Thread version
    Test: N/A
    Change-Id: I709f24040d1d1aa3c83df66a13b4084de8de100d

--- src/android/openthread-core-android-config.h
+++ src/android/openthread-core-android-config.h
@@ -330,7 +330,7 @@ static_assert(OPENTHREAD_CONFIG_NUM_MESSAGE_BUFFERS * (32 * sizeof(void *)) ==
 // Enable tracking the uptime of OpenThread instance.
 #define OPENTHREAD_CONFIG_UPTIME_ENABLE 1
 
-// The Thread version is 1.3
+// The Thread version is 1.4
 #define OPENTHREAD_CONFIG_THREAD_VERSION OT_THREAD_VERSION_1_4
 
 // Thread 1.2 Domain Unicast Address feature is not supported.
